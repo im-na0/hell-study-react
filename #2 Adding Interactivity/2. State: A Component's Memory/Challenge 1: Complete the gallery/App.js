cb549849a458787c96ed3aa7fe924384c5eef8f9
imnayoung <myblu1008@gmail.com>
2023-09-05 18:18:07 +0900

    Challenges: Complete the gallery

---
+++ #2 Adding Interactivity/2. State: A Component's Memory/Challenge 1: Complete the gallery/App.js
@@ -0,0 +1,50 @@
+// 더 좋은 패턴: 이벤트 핸들러 함수가 렌더링하는 동안 선언된 모든 변수를 “클로저로 참조”하도록 함
+//   let hasPrev = index > 0;
+//   let hasNext = index < sculptureList.length - 1;
+
+import { useState } from "react";
+import { sculptureList } from "./data.js";
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    if (index < sculptureList.length - 1) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+  function handlePerviousClick() {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handlePerviousClick} disabled={index === 0}>
+        Pervious
+      </button>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
+  );
+}
